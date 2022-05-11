64d246e711f2f5c7c6d728095ca8ca0bd09d7159
Obrienjames34 <Obrienjames934@gmail.com>
2022-05-11 12:02:05 -0400

    removed codes

--- assets/script.js
+++ assets/script.js
@@ -78,44 +78,7 @@ $("#task-form-modal .btn-primary").click(function() {
       date: taskDate
     });
 
-    $(".list-group").on("click", "p", function() {
-      $(".list-group").on("blur", "textarea", function() {
-        // get the textarea's current value/text
-var text = $(this)
-.val()
-.trim();
-
-// get the parent ul's id attribute
-var status = $(this)
-.closest(".list-group")
-.attr("id")
-.replace("list-", "");
-
-// get the task's position in the list of other li elements
-var index = $(this)
-.closest(".list-group-item")
-.index();
-tasks[status][index].text = text;
-saveTasks();
-// recreate p element
-var taskP = $("<p>")
-  .addClass("m-1")
-  .text(text);
-
-// replace textarea with p element
-$(this).replaceWith(taskP);
-
-
-      });
-      var text = $("this")
-      .text() 
-      var textInput = $("<textarea>")
-      $(this).replaceWith(textInput)
-      textInput.trigger("focus")
-      .addClass("form-control")
-      .val(text)
-      .trim()
-    });
+    saveTasks();
   }
 });
 
@@ -130,24 +93,3 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
-
-
-// due date was clicked
-$(".list-group").on("click", "span", function() {
-  // get current text
-  var date = $(this)
-    .text()
-    .trim();
-
-  // create new input element
-  var dateInput = $("<input>")
-    .attr("type", "text")
-    .addClass("form-control")
-    .val(date);
-
-  // swap out elements
-  $(this).replaceWith(dateInput);
-
-  // automatically focus on new element
-  dateInput.trigger("focus");
-});
