79382826bdb288bc87ab80d60c91b56e158b91cc
Obrienjames34 <Obrienjames934@gmail.com>
2022-05-11 11:49:45 -0400

    removed code

--- assets/script.js
+++ assets/script.js
@@ -132,3 +132,22 @@ $("#remove-tasks").on("click", function() {
 loadTasks();
 
 
+// due date was clicked
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this)
+    .text()
+    .trim();
+
+  // create new input element
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+
+  // swap out elements
+  $(this).replaceWith(dateInput);
+
+  // automatically focus on new element
+  dateInput.trigger("focus");
+});
